222faed3c51e921f00dc859a495dfbb115150f13
Heorhii Lazarevich <webremake@gmail.com>
2023-04-18 16:59:36 +0300

    implement test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,14 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_control():
-    browser.config.hold_browser_open = True
+    browser.config.hold_browser_open = False
     browser.config.browser_name = 'chrome'
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = '1024'
+    browser.config.base_url = 'https://github.com/'
+    browser.config.window_width = '1240'
     browser.config.window_height = '768'
     browser.config.timeout = 6.0
     yield
-    ...
+    browser.close()
+    browser.quit()
+
 
--- tests/test_name.py
+++ tests/test_name.py
@@ -1,5 +1,18 @@
 from selene import browser, have, be
 
 
-def test_function_name(browser_control):
-    pass
+def test_github_find_repo_issue_by_number(browser_control):
+    """
+    открываем https://github.com/
+    в строку поиска вводим `/webremake/wrm_selene_python_hw_7`
+    нажимаем Enter
+    на открывшейся странице клакаем на первую ссыдку c текстом `webremake/wrm_selene_python_hw_7`
+    кликаем на таб `Issues`
+    проверяем что в списке есть issue с номером `#2`
+    """
+    browser.open('/')
+    browser.element('[name=q]').should(be.clickable).type('webremake/wrm_selene_python_hw_7').press_enter()
+    browser.element('[href="/webremake/wrm_selene_python_hw_7"]').should(be.clickable).click()
+    browser.element('#issues-tab').should(be.clickable).click()
+    assert browser.element('.opened-by').should(have.text('#2'))
+
